9e9dca5f1a4390572ec57b15657fdaf12767a796
Wilson <91274932+WilsonPercival@users.noreply.github.com>
2022-11-29 06:47:36 +0200

    Update setters_instead_of_methods.js

--- setters instead of methods/setters_instead_of_methods.js
+++ setters instead of methods/setters_instead_of_methods.js
@@ -1,16 +1,30 @@
 ## Problem description
 
+Why setters instead of methods? After all, I am constantly mistaken, for example, writing
 
+`runtime.layout.scrolX = 13;`
+
+instead of
+
+`runtime.layout.scrollX = 13;`
+
+If there was a method instead of a setter, I would see an error in the console:
+
+`runtime.layout.scrolX(13);` - there is no such method, great! I don't spend a lot of time looking for a bug.
+
+You've already done well by adding the `scrollTo(x, y)` method, but what about the rest? `instance.width` instead of `instance.setWidth()`, `text.fontFace` instead of `text.setFontFace()`. Please tell me how can I get rid of the waste of time when on writing the wrong setter name I'm just looking for where I went wrong?
 
 ## Attach a .c3p
 
-
+There is not.
 
 ## Steps to reproduce
 
 
 
 ## Observed result
+
+`runtime.layout.scrolX`
 
 
 
